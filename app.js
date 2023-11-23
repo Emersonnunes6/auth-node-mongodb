399c95db013bbc7265599b6113c96191c4bbd53c
emersonnunes6 <enunessena@gmail.com>
2023-11-23 15:06:47 -0300

    ajuste

--- app.js
+++ app.js
@@ -7,13 +7,13 @@ const cors = require("cors");
 
 const app = express();
 
+app.use(express.json());
+
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   app.use(cors());
   next();
 });
-
-app.use(express.json());
 
 const User = require("./models/User");
 
