ca3b3d7d63296cc3616d8339f753afe4fa9a776c
emersonnunes6 <enunessena@gmail.com>
2023-11-23 13:22:47 -0300

    ajuste

--- app.js
+++ app.js
@@ -142,6 +142,27 @@ const dbUser = process.env.DB_USER;
 const dbPassword = process.env.DB_PASS;
 const tokenDB = `mongodb+srv://${dbUser}:${dbPassword}@cluster0.lyj5bgl.mongodb.net/`;
 
+module.exports = {
+    trailingSlash: false,
+    async headers() {
+      return [
+        {
+          // matching all API routes
+          source: '/:path*',
+          headers: [
+            { key: 'Access-Control-Allow-Credentials', value: 'true' },
+            { key: 'Access-Control-Allow-Origin', value: '*' },
+            { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
+            { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization' },
+          ],
+        },
+      ];
+    },
+    async redirects() {
+      return [];
+    }
+  };
+
 mongoose
   .connect(tokenDB)
   .then(() => {
