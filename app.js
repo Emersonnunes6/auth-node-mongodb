1ffcfc32846b8d74abd57d79e26b99061f7a6cfe
emersonnunes6 <enunessena@gmail.com>
2023-11-23 17:06:22 -0300

    ajuste cors

--- app.js
+++ app.js
@@ -9,12 +9,14 @@ const app = express();
 
 app.use(express.json());
 
-/* app.use((req, res, next) => {
+app.use((req, res, next) => {
+	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
     res.header("Access-Control-Allow-Origin", "*");
+	//Quais são os métodos que a conexão pode realizar na API
     res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
     app.use(cors());
     next();
-}); */
+});
 
 const User = require("./models/User");
 
@@ -94,7 +96,7 @@ app.get("/user/:id", checkToken, async (req, res) => {
   res.status(200).json(user);
 });
 
-app.post("/auth/register", cors(), async (req, res) => {
+app.post("/auth/register", async (req, res) => {
   const { username, email, password, confirmpassword } = req.body;
 
   if (!username) {
